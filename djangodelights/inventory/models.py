e6eff491919dc694f179b671f3f389c8b417fe79
nicho <kanwchan0@gmail.com>
2022-09-08 18:55:44 -0400

    Added Ingredient model

--- djangodelights/inventory/models.py
+++ djangodelights/inventory/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+    name = models.charField(max_length=200, unique=True)
+    quantity = models.FloatField(default=0)
+    unit = models.charField(max_length=200)
+    unit_price = models.FloatField(default=0)
