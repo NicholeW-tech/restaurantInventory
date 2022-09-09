e672d104e015b186ffbe4f4cf46eba9b185c9ef9
nicho <kanwchan0@gmail.com>
2022-09-09 18:20:21 -0400

    Created a create url for Ingredient model

--- djangodelights/inventory/urls.py
+++ djangodelights/inventory/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('ingredient/list', views.IngredientList.as_view(), name='ingredient_list'),
     path('ingredient/delete/<pk>', views.IngredientDelete.as_view(), name='ingredient_delete'),
     path('ingredient/update/<pk>', views.IngredientUpdate.as_view(), name='ingredient_update'),
+    path('ingredient/create', views.IngredientCreate.as_view(), name='ingredient_create'),
     path('menu_item/list', views.MenuItemList.as_view(), name='menu_item_list'),
 
 ]
