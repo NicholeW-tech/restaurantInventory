8dd0d468e3a60d51005f556874dc7bfdfde6c0fb
nicho <kanwchan0@gmail.com>
2022-09-09 23:33:46 -0400

    Created forms.py and added Ingredients form

--- djangodelights/inventory/views.py
+++ djangodelights/inventory/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, TemplateView
+
+from .forms import IngredientForm
 from .models import Ingredient, MenuItem, RecipeRequirement, Purchase
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.db.models import Sum
