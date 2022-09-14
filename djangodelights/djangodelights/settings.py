667540c0813fbacdc0296e4456c001c0429c67a1
nicho <kanwchan0@gmail.com>
2022-09-14 17:57:10 -0400

    Updated requirements.txt and settings for heroku deployment

--- djangodelights/djangodelights/settings.py
+++ djangodelights/djangodelights/settings.py
@@ -13,13 +13,15 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import django_heroku
+django_heroku.settings(locals())
 
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+IS_HEROKU = "DYNO" in os.environ
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -29,7 +31,11 @@ SECRET_KEY = os.getenv('D_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+if IS_HEROKU:
+    ALLOWED_HOSTS = ["*"]
+else:
+    ALLOWED_HOSTS = []
+
 
 
 # Application definition
